5396e91d84de3f1ec1d9e5a8a7e06e759e9336f5
victor <victord.1125@gmail.com>
2023-07-21 22:08:27 -0500

    feat: add s3 buckets

--- app.py
+++ app.py
@@ -7,22 +7,15 @@ from globant_challenge.globant_challenge_stack import GlobantChallengeStack
 
 
 app = cdk.App()
-GlobantChallengeStack(app, "GlobantChallengeStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+import aws_cdk as cdk
+from globant_challenge.cicd_stack import CICDSTack
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+app = cdk.App()
+CICDSTack(app, "CICDStack",
+    env=cdk.Environment(account="727474809098", region="eu-west-2")
+)
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+app.synth()
 
 app.synth()
--- globant_challenge/globant_challenge_stack.py
+++ globant_challenge/globant_challenge_stack.py
@@ -1,19 +1,32 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    Stack
 )
 from constructs import Construct
 
 class GlobantChallengeStack(Stack):
-
+    
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        hr_bucket = s3.Bucket(
+            scope,
+            "hr_data_globant",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            enforce_sSL=True,
+            versioned=True,
+            bucket_name="hr-data-globant"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "GlobantChallengeQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        landing_bucket = s3.Bucket(
+            scope,
+            "landing_bucket",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            enforce_sSL=True,
+            versioned=True,
+            bucket_name="hr-data-globant"
+        )
+
+
